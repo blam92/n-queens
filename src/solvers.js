80f6179c4eabdc8651a65c84155c9f191d8a6af8
blam92 <iraola.santi@gmail.com>
2017-12-07 15:26:40 -0800

    speed improvement by not creating entirely new array of arrays to pass to our Board constructor.

--- src/solvers.js
+++ src/solvers.js
@@ -17,7 +17,7 @@
 
 window.findNRooksSolution = function(n) {
 
-  var solution = new Board({'n':n}); 
+  var solution = new Board({'n': n}); 
   for (var i = 0; i < n; i++) {
     for (var j = 0; j < n; j++) {
       solution.togglePiece(i, j);
@@ -36,25 +36,28 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
 
-  var makeIndependentBoard = function(board) {
-    var arrayOfArrays = [];
-    for (var i = 0; i < board.get('n'); i++) {
-      arrayOfArrays.push(board.get(i).slice());
-    }
-    return arrayOfArrays;
-  };
-
   var solutionCount = 0;
   var board = new Board({'n': n});
   var makeABoard = function(board, indexRow) {
     for (var i = 0; i < n; i++) {
-      var tempBoard = new Board(makeIndependentBoard(board));
+      if (board.rows()[indexRow][i - 1] === 1) {
+        tempBoard.togglePiece(indexRow, i - 1);
+      }
+      var tempBoard = new Board(board.rows());
       tempBoard.togglePiece(indexRow, i);
       if (!tempBoard.hasAnyColConflicts(i)) {
         if (indexRow + 1 === n) {
-          solutionCount++;
+          solutionCount++;          
         } else {
           makeABoard(tempBoard, indexRow + 1);
+        }
+      }
+      tempBoard.togglePiece(indexRow, i);
+        if ( i === n ) {
+        if (tempBoard._isInBounds(indexRow - 1, i)) {
+          if (tempBoard.rows()[indexRow - 1][i] === 1) {
+            tempBoard.togglePiece(indexRow, i - 1);
+          }
         }
       }
     }       
@@ -106,33 +109,39 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var makeIndependentBoard = function(board) {
-    var arrayOfArrays = [];
-    for (var i = 0; i < board.get('n'); i++) {
-      arrayOfArrays.push(board.get(i).slice());
-    }
-    return arrayOfArrays;
-  };
-
+  
   var solutionCount = 0;
   var board = new Board({'n': n});
   var makeABoard = function(board, indexRow) {
     for (var i = 0; i < n; i++) {
-      var tempBoard = new Board(makeIndependentBoard(board));
+      if (board.rows()[indexRow][i - 1] === 1) {
+        tempBoard.togglePiece(indexRow, i - 1);
+      }
+      var tempBoard = new Board(board.rows());
       tempBoard.togglePiece(indexRow, i);
       if (!tempBoard.hasAnyColConflicts(i) && !tempBoard.hasAnyMajorDiagonalConflicts() && !tempBoard.hasAnyMinorDiagonalConflicts()) {
         if (indexRow + 1 === n) {
-          solutionCount++;
+          solutionCount++;          
         } else {
           makeABoard(tempBoard, indexRow + 1);
         }
       }
+      tempBoard.togglePiece(indexRow, i);
+        if ( i === n ) {
+        if (tempBoard._isInBounds(indexRow - 1, i)) {
+          if (tempBoard.rows()[indexRow - 1][i] === 1) {
+            tempBoard.togglePiece(indexRow, i - 1);
+          }
+        }
+      }
     }       
   };
+
   if (n === 0) {
     return 1;
   }
   makeABoard(board, 0);
+
   return solutionCount;
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
 };
